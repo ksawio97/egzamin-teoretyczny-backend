0b931a079659bcf17eec4f17277ab812164e63b4
Ksawery Kuczynski <ksawerykuczynskikk@gmail.com>
2024-06-04 22:52:39 +0200

    Added docker compose config

--- db.js
+++ db.js
@@ -3,7 +3,23 @@ const Grid = require('gridfs-stream');
 const { GridFsStorage } = require('multer-gridfs-storage');
 const multer = require('multer');
 
-const dbURI = 'mongodb://localhost:27017/egzaminteoretycznydb'; // Replace with your MongoDB URI
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+} = process.env;
+
+
+const login = (() => {
+  if (!DB_USER || !DB_PASSWORD)
+      return '';
+  return `${DB_USER}:${DB_PASSWORD}@`;
+})();
+
+// Replace with your MongoDB URI
+const dbURI = `mongodb://${login}${DB_HOST || 'localhost'}:${DB_PORT || '27017'}/${DB_NAME || 'egzaminteoretycznydb'}?authSource=admin`;
 
 // Connect to MongoDB
 mongoose.connect(dbURI)
